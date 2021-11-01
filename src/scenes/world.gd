15febe7d97b0c8f662ed700dc24936d3b48c0041
Samuel Šulovský <71935095+memoriesadrift@users.noreply.github.com>
2021-11-01 16:09:08 +0100

    Add Weather Timer (#4)

--- src/scenes/world.gd
+++ src/scenes/world.gd
@@ -3,9 +3,32 @@ extends Node2D
 enum Weather {SUNNY, RAINY, WINDY, SNOWY}
 
 var currentWeather: int = Weather.SUNNY
+var weatherEffectAccumulators: Dictionary = {
+    Weather.SUNNY: 0,
+    Weather.RAINY: 0,
+    Weather.WINDY: 0,
+    Weather.SNOWY: 0,
+   }
+onready var worldTimer: Timer = get_node("WorldTimer")
+
+func _ready() -> void:
+    worldTimer.set_wait_time(1)
+    worldTimer.start()
+
+func _on_WorldTimer_timeout() -> void:
+    _adjust_weatherDuration(currentWeather)
+    print(weatherEffectAccumulators)
 
 func _on_Player_song_played(song) -> void: # song MUST be dynamically typed
     _change_Weather(_song_signal_to_weather(song))
+
+# Helper function to adjust weather durations based on current weather
+func _adjust_weatherDuration(weather: int) -> void:
+    weatherEffectAccumulators[weather] += 1
+    
+    for key in weatherEffectAccumulators:
+        if(weather != key and weatherEffectAccumulators[key] > 0):
+            weatherEffectAccumulators[key] -= 1
 
 # Helper function to safely assign to currentWeather
 func _change_Weather(to: int) -> void:
