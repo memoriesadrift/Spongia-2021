0dc9892ef2c3c02365f9e837beafb1eb926a60b0
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-11-01 15:46:10 +0100

    Add timer to world, track weather durations

--- src/scenes/world.gd
+++ src/scenes/world.gd
@@ -3,9 +3,40 @@ extends Node2D
 enum Weather {SUNNY, RAINY, WINDY, SNOWY}
 
 var currentWeather: int = Weather.SUNNY
+var weatherDurations: Dictionary = {
+    Weather.SUNNY: 0,
+    Weather.RAINY: 0,
+    Weather.WINDY: 0,
+    Weather.SNOWY: 0,
+   }
+onready var worldTimer: Timer = get_node("WorldTimer")
+
+func _ready() -> void:
+    worldTimer.set_wait_time(1)
+    worldTimer.start()
+
+func _on_WorldTimer_timeout() -> void:
+    _adjust_weatherDuration(currentWeather)
 
 func _on_Player_song_played(song) -> void: # song MUST be dynamically typed
     _change_Weather(_song_signal_to_weather(song))
+
+# Helper function to adjust weather durations based on current weather
+func _adjust_weatherDuration(weather: int) -> void:
+    weatherDurations[weather] += 1
+    
+    if (weather != Weather.SUNNY):
+        if (weatherDurations[Weather.SUNNY] > 0):
+            weatherDurations[Weather.SUNNY] -= 1
+    if (weather != Weather.RAINY):
+        if (weatherDurations[Weather.RAINY] > 0):
+            weatherDurations[Weather.RAINY] -= 1
+    if (weather != Weather.WINDY):
+        if (weatherDurations[Weather.WINDY] > 0):
+            weatherDurations[Weather.WINDY] -= 1
+    if (weather != Weather.SNOWY):
+        if (weatherDurations[Weather.SNOWY] > 0):
+            weatherDurations[Weather.SNOWY] -= 1
 
 # Helper function to safely assign to currentWeather
 func _change_Weather(to: int) -> void:
