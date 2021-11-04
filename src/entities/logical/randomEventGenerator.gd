139c68c2d6c5ad1ce143a01535ee3655f2e0c15e
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-11-04 18:02:50 +0100

    Add some events, generate an event at most every second.

--- src/entities/logical/randomEventGenerator.gd
+++ src/entities/logical/randomEventGenerator.gd
@@ -4,8 +4,14 @@ signal random_event(event)
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 var bias: int = 0 # "Bad Luck Protection" to increase chance of generating event with time
+var accumulatedDelta: float = 0
+const events = ["fire", "invaders", "hail"]
 
 func _process(delta: float) -> void:
+    accumulatedDelta += delta
+    if (accumulatedDelta < 1):
+        return
     var eventGenerated: int = rng.randi_range(0, 100 - bias)
     if (!eventGenerated):
-        emit_signal("random_event", "event")
+        var event: String = events[rng.randi_range(0, events.size() -1)]
+        emit_signal("random_event", event)
