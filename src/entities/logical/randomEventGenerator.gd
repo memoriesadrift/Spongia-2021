12523ce92e108f6e263cd88b8d1776425a15f040
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-11-05 18:31:01 +0100

    Fix bug in accumulatedDelta not being reset

--- src/entities/logical/randomEventGenerator.gd
+++ src/entities/logical/randomEventGenerator.gd
@@ -12,11 +12,14 @@ func _process(delta: float) -> void:
     accumulatedDelta += delta
     if (accumulatedDelta < 1 or isEventHappening):
         return
+    accumulatedDelta = 0
     var eventGenerated: int = rng.randi_range(0, 100 - bias)
     if (!eventGenerated):
         var event: String = events[rng.randi_range(0, events.size() -1)]
         isEventHappening = true
         emit_signal("random_event", event)
+        print("event fired!")
+        print(event)
 
 
 func _on_World_random_event_complete() -> void:
